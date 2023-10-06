d39f5be935aa595a97546267336aabbaba83aaf1
baugxx <145669377+baugxx@users.noreply.github.com>
2023-10-06 01:16:04 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'34f51bde-10b2-4367-af38-397254d5ae03'}
 WSPATH=${WSPATH:-'choreo'}
 
 # 生成配置文件，并经过 base64 加密
@@ -87,7 +87,7 @@ echo "{
                 }
             },
             \"sniffing\":{
-                \"enabled\":true,
+                \"enabled\":false,
                 \"destOverride\":[
                     \"http\",
                     \"tls\"
@@ -114,7 +114,7 @@ echo "{
                 }
             },
             \"sniffing\":{
-                \"enabled\":true,
+                \"enabled\":false,
                 \"destOverride\":[
                     \"http\",
                     \"tls\"
@@ -141,7 +141,7 @@ echo "{
                 }
             },
             \"sniffing\":{
-                \"enabled\":true,
+                \"enabled\":false,
                 \"destOverride\":[
                     \"http\",
                     \"tls\"
@@ -169,7 +169,7 @@ echo "{
                 }
             },
             \"sniffing\":{
-                \"enabled\":true,
+                \"enabled\":false,
                 \"destOverride\":[
                     \"http\",
                     \"tls\"
